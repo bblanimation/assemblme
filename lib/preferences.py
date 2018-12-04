69ce3a0b4dc15bf675bc7cb65233488d9a8854af
bblanimation <cgear13@gmail.com>
2018-12-04 01:56:57 -0500

    Updated license block formatting

--- lib/preferences.py
+++ lib/preferences.py
@@ -1,34 +1,19 @@
-bl_info = {
-    "name"        : "AssemblMe",
-    "author"      : "Christopher Gearhart <chris@bblanimation.com>",
-    "version"     : (1, 2, 0),
-    "blender"     : (2, 79, 0),
-    "description" : "Iterative object assembly animations made simple",
-    "location"    : "View3D > Tools > AssemblMe",
-    "wiki_url"    : "https://www.blendermarket.com/products/assemblme",
-    "tracker_url" : "https://github.com/bblanimation/assemblme/issues",
-    "category"    : "Animation"}
-
-"""
-Copyright (C) 2017 Bricks Brought to Life
-http://bblanimation.com/
-chris@bblanimation.com
-
-Created by Christopher Gearhart
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# Copyright (C) 2018 Christopher Gearhart
+# chris@bblanimation.com
+# http://bblanimation.com/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # system imports
 import bpy
