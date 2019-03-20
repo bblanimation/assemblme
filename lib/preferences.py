a64d51267337e9e06156af8ee6d045d65d076dc5
bblanimation <cgear13@gmail.com>
2019-03-20 11:55:21 -0500

    further bug fixes

--- lib/preferences.py
+++ lib/preferences.py
@@ -49,24 +49,24 @@ class ASSEMBLME_PT_preferences(AddonPreferences):
             default=False)
 
 	# addon updater preferences
-    auto_check_update = bpy.props.BoolProperty(
+    auto_check_update = BoolProperty(
         name = "Auto-check for Update",
         description = "If enabled, auto-check for updates using an interval",
         default = False)
-    updater_intrval_months = bpy.props.IntProperty(
+    updater_intrval_months = IntProperty(
         name='Months',
         description = "Number of months between checking for updates",
         default=0, min=0)
-    updater_intrval_days = bpy.props.IntProperty(
+    updater_intrval_days = IntProperty(
         name='Days',
         description = "Number of days between checking for updates",
         default=7, min=0)
-    updater_intrval_hours = bpy.props.IntProperty(
+    updater_intrval_hours = IntProperty(
         name='Hours',
         description = "Number of hours between checking for updates",
         min=0, max=23,
         default=0)
-    updater_intrval_minutes = bpy.props.IntProperty(
+    updater_intrval_minutes = IntProperty(
         name='Minutes',
         description = "Number of minutes between checking for updates",
         min=0, max=59,
